e728a2e3454195b06d2542b262d8aca4c8ba6dd6
Wanderson Soares <wandersonsoares841@gmail.com>
2024-09-03 20:55:39 -0300

    feat: add solution

--- q3.py
+++ q3.py
@@ -1,10 +1,46 @@
+import json
 
-with open("data.json") as file: data = json.load(file)
+with open('data.json') as file:
+    data = json.load(file)
 
-month = 'setembro'
+def min_income():
+    min = data[0]['valor']
+    for d in data:
+        if (d['valor'] < min):
+            min = d['valor']
+    return min
 
-print("Dias totais de {}: {}".format(month, len(data[month])));
+def max_income():
+    max = data[0]['valor']
+    for d in data:
+        if (d['valor'] > max):
+            max = d['valor']
+    return max
 
+def average_income():
+    sum = 0.0
+    count = 0
+    for d in data:
+        if (d['valor'] != 0):
+            count += 1
+            sum += d['valor']
+    if (count == 0):
+        return 0.0
+    return sum / count
 
+print("Dias totais: {}".format(len(data)));
 
+print("Menor valor de faturamento: {}".format(min_income()))
+
+print("Maior valor de faturamento: {}".format(max_income()))
+
+avg = average_income()
+
+days = 0
+
+for d in data:
+    if (d['valor'] > avg):
+        days += 1
+
+print("Em {} dias o faturamento diário excedeu a média mensal de {}".format(days, avg))
 
